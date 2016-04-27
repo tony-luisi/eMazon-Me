97ddedb71ed97d1b151820a3192220b8bb19930f
Keakone <keanucarnevale@gmail.com>
2016-04-28 11:46:37 +1200

    adjusted package.json, added start script

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var http = require('http')
+var bodyParser = require('body-parser')
+var path = require('path')
+var port = process.env.PORT || 8080
+
+var app = express()
+var server = http.createServer(app)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req,res) {
+	res.send('wassup dawg')
+})
+
+if (require.main === module) {
+  server.listen(port, function () {
+    console.log('http server listening on port: ', port)
+  })
+}
