89a5df5a129e81611602ecd1df25a8aa7bfa091c
Rawad Alawar <ralawar54@gmail.com>
2016-04-28 17:43:59 +1200

    generic-ize the storeFront thumbnails

---
+++ components/FrontPage.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import StoreTN from './StoreTN'
+
+class FrontPage extends Component{
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+        <div className="blah">
+            <h2>eMazon-Me</h2>
+          <div className='search'>
+              <form>
+                <input className= 'search-bar' type="text" placeholder="Search..." required />
+                <input className= 'search-bttn' type="button" value="Search" />
+              </form>
+            </div>
+            {this.props.storeTNs.map( (storeTN) =>
+              <StoreTn name={storeTN.name} items={storeTN.items} logo={storeTN.logo} />)}
+
+        </div>
+    )
+  }
+}
+
+export default FrontPage
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 // import App from './components/app'
-import FrontPage from './components/frontPage'
-
+import FrontPage from './components/FrontPage'
+import {storeTNs} from './data/sampleData'
 //render(<App name='eMazon-Me' />, document.querySelector('main'))
-render(<FrontPage />, document.querySelector('main'))
+render(<FrontPage storeTNs={storeTNs}/>, document.querySelector('body'))
 console.log('welcome to eMazon-Me')
