d3696df975e3caf80a357b583984a0cd5823de6e
tony-luisi <t.luisi@gmail.com>
2016-04-29 15:46:16 +1200

    database update client

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import StoresListings from './StoresListings'
+import request from 'superagent'
 
 import testData from '.././data/thumbnailSampleData'
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { testData: testData }
   }
+
+  componentDidMount(){
+    request.get('/stores').end(function(err, res){
+      console.log('error', err)
+      var storeData = JSON.parse(res.text)
+      this.setState({ testData: storeData })
+    }.bind(this))
+  }
+
 
   render () {
     return (
